2d2498c73846c2de31e8b926d077fff7c30bb246
bishoco <bishoco@gmail.com>
2019-12-04 09:20:52 -0500

    site is now feature complete

--- foolnews/articles/views.py
+++ foolnews/articles/views.py
@@ -1,8 +1,8 @@
 from datetime import datetime
 import random
 
-from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponseRedirect, Http404
 from django.urls import reverse
 from django.utils.html import strip_tags
 
@@ -13,11 +13,19 @@ def index(request):
     #pull the main article slug from the ContentConfig table
     #It didn't feel right to hard code "10-promise" so I created a 
     #config table that could be updated in the admin
-    slug = ContentConfig.objects.get(key = "main-article-slug").value
-    sub_article_count = 3 #number of articles listed at the bottom of the main page
+    slug_config = get_object_or_404(ContentConfig, key="main-article-slug")
+    slug = slug_config.value
+
+    #raise a 404 if the article is not found
+    if not slug:
+        raise Http404("Main article slug not set in admin")
+
+    #number of articles listed at the bottom of the main page
+    sub_article_count = 3 
 
     article_list = get_articles_from_api()
     main_article = get_main_article (article_list, slug);
+    
     sub_article_list = get_sub_articles(article_list, sub_article_count, main_article.uuid)
 
     context = {'main_article': main_article, 'sub_article_list': sub_article_list}
@@ -26,6 +34,11 @@ def index(request):
 def detail(request, uuid):
     article_list = get_articles_from_api()
     raw_article = get_article_by_uuid(article_list, uuid)
+    
+    #raise a 404 if the article is not found
+    if not raw_article:
+        raise Http404("Article not found")
+
     instruments = raw_article.get("instruments")
     
     article = convert_raw_article_to_article(raw_article)
@@ -34,9 +47,10 @@ def detail(request, uuid):
     stock_list = filter_stock_list_by_instruments(stock_list, instruments)
 
     comment_list = Comment.objects.filter(article_uuid=uuid).order_by('-comment_date')
-    print(comment_list)
 
-    context = {'article': article, "stock_list": stock_list, "comment_list": comment_list}
+    read_more_list = get_sub_articles(article_list, 3, '')
+
+    context = {'article': article, "stock_list": stock_list, "comment_list": comment_list, "read_more_list": read_more_list}
     return render(request, 'articles/detail.html', context)
 
 def comment(request, uuid):
