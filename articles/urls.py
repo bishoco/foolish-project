c18798d9ee7f02fe7c86988c80c3ce0fc20ac627
bishoco <bishoco@gmail.com>
2020-01-07 18:42:52 -0500

    Converted to class based views

--- articles/urls.py
+++ articles/urls.py
@@ -4,8 +4,8 @@ from . import views
 
 app_name = 'articles'
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('detail/<str:uuid>', views.detail, name='detail'),
+    path('', views.MainView.as_view(), name='index'),
+    path('detail/<str:uuid>', views.ArticleDetailView.as_view(), name='detail'),
     path('detail/<str:uuid>/comment', views.comment, name='comment'),
     path('search', views.search, name='search')
 ]
--- articles/views.py
+++ articles/views.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 import random
 
+from django.views.generic.base import TemplateView
 from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponseRedirect, Http404
 from django.urls import reverse
@@ -9,50 +10,65 @@ from django.utils.html import strip_tags
 from .models import Article, Comment, ContentConfig
 from .utils import *
 
-def index(request):
-    #pull the main article slug from the ContentConfig table
-    #It didn't feel right to hard code "10-promise" so I created a 
-    #config table that could be updated in the admin
-    slug_config = get_object_or_404(ContentConfig, key="main-article-slug")
-    slug = slug_config.value
+class MainView(TemplateView):
+    template_name = "articles/index.html"
 
-    #raise a 404 if the article is not found
-    if not slug:
-        raise Http404("Main article slug not set in admin")
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
 
-    #number of articles listed at the bottom of the main page
-    sub_article_count = 3 
+        #pull the main article slug from the ContentConfig table
+        #It didn't feel right to hard code "10-promise" so I created a 
+        #config table that could be updated in the admin
+        slug_config = get_object_or_404(ContentConfig, key="main-article-slug")
+        slug = slug_config.value
 
-    article_list = get_articles_from_api()
-    main_article = get_main_article (article_list, slug);
-    
-    sub_article_list = get_sub_articles(article_list, sub_article_count, main_article.uuid)
+        #raise a 404 if the article is not found
+        if not slug:
+            raise Http404("Main article slug not set in admin")
 
-    context = {'main_article': main_article, 'sub_article_list': sub_article_list}
-    return render(request, 'articles/index.html', context)
+        #number of articles listed at the bottom of the main page
+        sub_article_count = 3 
 
-def detail(request, uuid):
-    article_list = get_articles_from_api()
-    raw_article = get_article_by_uuid(article_list, uuid)
-    
-    #raise a 404 if the article is not found
-    if not raw_article:
-        raise Http404("Article not found")
+        article_list = get_articles_from_api()
+        main_article = get_main_article (article_list, slug);
+        
+        sub_article_list = get_sub_articles(article_list, sub_article_count, main_article.uuid)
 
-    #list of instruments used to filter stock list
-    instruments = raw_article.get("instruments")
-    
-    article = convert_raw_article_to_article(raw_article)
+        context['main_article'] = main_article
+        context['sub_article_list'] = sub_article_list
+        return context
 
-    stock_list = get_stocks_from_api()
-    stock_list = filter_stock_list_by_instruments(stock_list, instruments)
+class ArticleDetailView(TemplateView):
+    template_name = "articles/detail.html"
 
-    comment_list = Comment.objects.filter(article_uuid=uuid).order_by('-comment_date')
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
 
-    read_more_list = get_sub_articles(article_list, 3, '')
+        uuid = kwargs['uuid']
+        article_list = get_articles_from_api()
+        raw_article = get_article_by_uuid(article_list, uuid)
+        
+        #raise a 404 if the article is not found
+        if not raw_article:
+            raise Http404("Article not found")
 
-    context = {'article': article, "stock_list": stock_list, "comment_list": comment_list, "read_more_list": read_more_list}
-    return render(request, 'articles/detail.html', context)
+        #list of instruments used to filter stock list
+        instruments = raw_article.get("instruments")
+        
+        article = convert_raw_article_to_article(raw_article)
+
+        stock_list = get_stocks_from_api()
+        stock_list = filter_stock_list_by_instruments(stock_list, instruments)
+
+        comment_list = Comment.objects.filter(article_uuid=uuid).order_by('-comment_date')
+
+        read_more_list = get_sub_articles(article_list, 3, '')
+
+        context['article'] = article
+        context['stock_list'] = stock_list
+        context['comment_list'] = comment_list
+        context['read_more_list'] = read_more_list
+        return context
 
 def comment(request, uuid):
     comment = Comment(
